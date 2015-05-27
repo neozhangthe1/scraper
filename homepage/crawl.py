859af1658dfcfb368e99f441d0cfb21f8206e8d7
neozhangthe1 <stack@live.cn>
2015-05-27 16:55:34 +0800

    crawl:

--- homepage/crawl.py
+++ homepage/crawl.py
@@ -8,8 +8,10 @@ print "data query finished"
 
 cnt = 0
 for item in data:
+	print item["_id"], item["name"]
 	hp = item["contact"]["homepage"]
 	try:
+		print hp
 		res = requests.get(hp)
 		f_out = open(item["_id"] + ".html")
 		f_out.write(res.text)
