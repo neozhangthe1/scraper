d16b7ac7e5d14ce825a63c0226beac3cd6c02f82
neozhangthe1 <stack@live.cn>
2016-04-10 21:49:31 +0800

    debug youzy

--- botify/youzy/youzy/spiders/youzy.py
+++ botify/youzy/youzy/spiders/youzy.py
@@ -48,9 +48,8 @@ class YouzySpider(CrawlSpider):
     def get_urls(self, response):
         urls = response.xpath("//div[@class='major-list-con']/ul/li/a/@href").extract()
         urls = ["http://www.youzy.cn" + u for u in urls]
-        for u in urls:
-            yield Request(u, callback=self.parse)
         return urls
+
 
     def parse(self, response):
         """
@@ -58,32 +57,37 @@ class YouzySpider(CrawlSpider):
         start_urls attribute of the Spider, and assigns them the parse method
         of the spider as their callback function.
         """
-        if response.url == 'http://www.youzy.cn/major/index':
-            self.get_urls(response)
+        print(response.url)
+        if 'index' in response.url:
+            urls = self.get_urls(response)
+            for u in urls:
+                yield Request(u, callback=self.parse)
 
-        for elem in response.xpath("//ul[@class='uzy-college-list']/li"):
-            d = {
-                "subject": response.xpath("//div[@class='major-right']/div[@class='major-info']/h3/text()").extract()[0],
-                "subject_info": response.xpath("//div[@class='major-right']/div[@class='major-info']/div[@class='info']/span/text()").extract()[0],
-                "name": elem.xpath("div/div[@class='top']/a/text()").extract()[0],
-                "name_en": elem.xpath("div[@class='mark']/a/img/@alt").extract()[0],
-                "img": elem.xpath("div[@class='mark']/a/img/@src").extract()[0],
-                "url":  "http://www.youzy.cn" + elem.xpath("div/div[@class='top']/a/@href").extract()[0],
-                "tags": elem.xpath("div/div/div[@class='tag']/img/@title").extract(),
-                "properties": elem.xpath("div/div[@class='bottom']/ul/li/text()").extract(),
-                "art": False
-            }
-            if len(elem.xpath("div/div[@class='bottom']/img")) > 0:
-                d["art"] = True
-            col.insert(d)
+        elif "opencollege" in response.url:
+            print(response.url)
+            for elem in response.xpath("//ul[@class='uzy-college-list']/li"):
+                d = {
+                    "subject": response.xpath("//div[@class='major-right']/div[@class='major-info']/h3/text()").extract()[0],
+                    "subject_info": response.xpath("//div[@class='major-right']/div[@class='major-info']/div[@class='info']/span/text()").extract()[0],
+                    "name": elem.xpath("div/div[@class='top']/a/text()").extract()[0],
+                    "name_en": elem.xpath("div[@class='mark']/a/img/@alt").extract()[0],
+                    "img": elem.xpath("div[@class='mark']/a/img/@src").extract()[0],
+                    "url":  "http://www.youzy.cn" + elem.xpath("div/div[@class='top']/a/@href").extract()[0],
+                    "tags": elem.xpath("div/div/div[@class='tag']/img/@title").extract(),
+                    "properties": elem.xpath("div/div[@class='bottom']/ul/li/text()").extract(),
+                    "art": False
+                }
+                if len(elem.xpath("div/div[@class='bottom']/img")) > 0:
+                    d["art"] = True
+                col.insert(d)
 
-            cur_page = 0
-            tmp = response.url.split("page=")
-            if len(tmp) > 1:
-                cur_page = int(tmp[1])
+                cur_page = 0
+                tmp = response.url.split("page=")
+                if len(tmp) > 1:
+                    cur_page = int(tmp[1])
 
-            pagination = response.xpath("//ul[@class='pagination']/li/a/text()").extract()
-            if len(pagination) > 0:
-                if int(pagination[-1]) != cur_page:
-                    yield Request(tmp[0] + "page=" + str(cur_page + 1), callback=self.parse)
+                pagination = response.xpath("//ul[@class='pagination']/li/a/text()").extract()
+                if len(pagination) > 0:
+                    if int(pagination[-1]) != cur_page:
+                        yield Request(tmp[0] + "page=" + str(cur_page + 1), callback=self.parse)
 
